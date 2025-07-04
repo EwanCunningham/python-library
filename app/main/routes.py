31b9d076446731f94a03dd108d1403074fc00a1f
Ubuntu <ubuntu@ip-172-31-44-150.eu-west-1.compute.internal>
2025-07-04 15:37:40 +0000

    Add login form

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app.main import bp 
+from app.login import LoginForm
 
 @bp.route('/')
 @bp.route('/index')
@@ -10,3 +11,12 @@ def index():
 @bp.route('/api')
 def api():
     return "Hello world!" 
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Login', form=form)
