9f3d94c445b311d720b629a9c8f459d7e97f70c5
Ubuntu <ubuntu@ip-172-31-44-150.eu-west-1.compute.internal>
2025-07-07 09:15:50 +0000

    move login code to blueprint

---
+++ app/login/routes.py
@@ -0,0 +1,12 @@
+from flask import render_template, flash, redirect
+from app.login import bp
+from app.login.loginForm import LoginForm
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Login', form=form)
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,5 @@
-from flask import render_template, flash, redirect
+from flask import render_template
 from app.main import bp 
-from app.login import LoginForm
 
 @bp.route('/')
 @bp.route('/index')
@@ -12,11 +11,3 @@ def index():
 def api():
     return "Hello world!" 
 
-@bp.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html', title='Login', form=form)
