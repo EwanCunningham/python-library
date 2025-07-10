c6b756121c77eab6a10c521f83d630e667a2845b
Ubuntu <ubuntu@ip-172-31-44-150.eu-west-1.compute.internal>
2025-07-10 10:50:45 +0000

    Add db, login, user profile and error handling

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,29 @@
+import logging, os
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from logging.handlers import RotatingFileHandler
 
-def create_app(config_class=Config):
-    app = Flask(__name__)
-    app.config.from_object(config_class)
+app = Flask(__name__)
+app.config.from_object(Config)
 
-    # Initialise Flask extensions
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-    # Register blueprints
-    from app.main import bp as main_bp
-    app.register_blueprint(main_bp)
+login = LoginManager(app)
+login.login_view = 'login'
 
-    from app.login import bp as login_bp
-    app.register_blueprint(login_bp)
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/library.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
 
-    return app
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Library startup')
+
+from app import routes, models, errors
---
+++ app/models.py
@@ -0,0 +1,40 @@
+from datetime import datetime, timezone
+from typing import Optional
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import db, login
+
+class User(UserMixin, db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
+    password_hash : so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    books: so.WriteOnlyMapped['Book'] = so.relationship(back_populates='owner')
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(default=lambda: datetime.now(timezone.utc))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Book(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    title: so.Mapped[str] = so.mapped_column(sa.String(140))
+    author: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+    owner: so.Mapped[User] = so.relationship(back_populates='books')
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
